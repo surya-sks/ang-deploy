a4a307e0c693e67c1ac122ca392504d8e20b100d
surya-sks <suryaskandasamy@gmail.com>
2024-07-22 17:00:49 +0530

    Auto-generated commit

--- server/chunk-7ESUIGI6.mjs
+++
@@ -1,3 +0,0 @@
-import './polyfills.server.mjs';
-import{a as t,b as r,c as p,d as n,i,l as m,n as f,o as a,r as s,v as c,y as d,z as l}from"./chunk-55YR2Z2N.mjs";var u=(()=>{class o{title="ang-deploy";static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-root"]],standalone:!0,features:[n],decls:2,vars:0,template:function(e,R){e&1&&(p(0,` test
-`),r(1,"router-outlet"))},dependencies:[c,l]})}return o})();var g=[];var v={providers:[i({eventCoalescing:!0}),d(g),a()]};var y={providers:[s()]},C=m(v,y);var A=()=>f(u,C),E=A;export{E as a};
--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-7ESUIGI6.mjs";import"./chunk-55YR2Z2N.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
