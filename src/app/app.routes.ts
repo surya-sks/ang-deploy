5c6b41aab7b3036ec4e79ee42180b8dbb995765e
surya-sks <suryaskandasamy@gmail.com>
2024-07-23 13:18:14 +0530

    dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: AppComponent,
-        title: 'App page',
-      },
       {
-        path: 'home',
+        path: '',
         component: HomeComponent,
         title: 'Home page',
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        title: 'Dashboard page',
       },
 ];
